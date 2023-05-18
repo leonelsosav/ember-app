37f96c128d6ca303fbd8b660954aaabad575518f
Leonel Vera Sosa <lvera@squareup.com>
2023-05-17 23:22:16 -0500

    Add change color function

--- app/components/product.js
+++ app/components/product.js
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class ProductComponent extends Component {}
+export default class ProductComponent extends Component {
+  @tracked color = 'red';
+
+  @action
+  onChangeColor(newColor) {
+    this.color = newColor;
+  }
+}
